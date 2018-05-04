ad517f53ff101c028b54eff3014a92a16f2a0ed4
Lucio Paiva <luciopaiva@gmail.com>
2018-05-04 11:23:43 -0300

    First automaton rules

--- README.md
+++ README.md
@@ -5,6 +5,75 @@ Water flowing experiment focused on keeping fluid leveled across interconnected 
 
 Although this has nothing to do with Dwarf Fortress per se (despite the fact that DF also simulates fluids), I decided to pay homage to it and play with its text-based interface.
 
+My first attempt is at trying to simulate it like cellular automata, i.e., each cell in the grid follows very strict rules based on its neighborhood to decide what should be its next state. Like one would expect of cellular automata systems, during its iteration a cell can only modify itself. I don't necessarily have to adhere to this rule, but doing so can be helpful if I decide to implement it as a WebGL shader, since I'm not sure I can modify other cell during execution (need to check this!).
+
+With that in mind, let's think about how water would flow.
+
+## Falling
+
+Consider this situation:
+
+  ? ? ?
+  ? ~ ?
+  ? . ?
+  ? ? ?
+
+Where `~` represents a water cell, `.` represents an empty space and `?` represents an irrelevant cell.
+
+When its time for `~` to step, the rule is: "if there is space below me, turn me into space". Complementary, `.` runs the rule: "if there is water above me, turn me into water". The next state should be:
+
+  ? ? ?
+  ? . ?
+  ? ~ ?
+  ? ? ?
+
+And the water effectively fell down.
+
+But now what about this state:
+
+ ? ? ? ? ?
+ ? ? ~ ? ?
+ # ~ . ~ #
+ # # # # #
+
+Here `#` is a solid wall. What happens to the three water tiles? In case the ones at the same level as the empty spot decide to take the empty spot, we can have a final state where more than one water tile decided to take the empty tile. If the simulation is flawed, it will make water disappear. Say that the left water tile rule is: "considering I can't go down, can I go sideways? If my flow direction is left and there's a wall there, change my direction to right. If my flow direction is right and there's an empty space, make me disappear". For the water tile to be present in the next state, the empty space rule would need to be: "check water tiles all around me. If there's water above me, make me water. If there's also water on my left side and its flow direction is right, make me water with weight 2. If there's water on my right side and it's coming my way too, make my weight 3". Resulting state:
+
+ ? ? ? ? ?
+ ? ? . ? ?
+ # . 3 . #
+ # # # # #
+
+Then, in the next step, that new water tile with weight 3 would need to relieve its excess water somehow. Maybe it would work, I guess. The rule would be: "if my weight is 3 and there's empty slots on both sides, make me weight 1". On the left empty tile, the rule would be complementary: "if there's a water tile weight 3 on my right, turn me into water with weight 1". The same rule would apply to the empty spot on the right side:
+
+ ? ? ? ? ?
+ ? ? . ? ?
+ # ~ ~ ~ #
+ # # # # #
+
+And then the system would enter an equilibrium state. All three water tiles would not be able to move anymore.
+
+Is it possible to simulate it by looking only at my closest 8 neighbors? What about this situation:
+
+    ~
+    ~
+    .
+
+The top water tile won't move since there's water below it. Maybe it'd try to move sideways (what would be even worse)? Anyway, consider it won't move. The bottom water tile decides to disappear, since there's space below it. The space decides to turn into water, since there's water above it:
+
+    ~
+    .
+    ~
+
+Weird state. In the next step the top water tile will finally start to fall. How can that be avoided?
+
+## Miscellaneous references
+
+Good simulation here: https://www.youtube.com/watch?v=tG2SP4F4dUc
+
+[This article](https://w-shadow.com/blog/2009/09/01/simple-fluid-simulation/) proposes using an idea of compressible water. A cell can temporarily store more water than it would under equilibrium. It would then try and get rid of its excess water in the next steps.
+
+[Here](http://www.gamasutra.com/view/feature/3549/interview_the_making_of_dwarf_.php?page=9) Tarn discusses how it handled U-shaped tubes by teleporting water from one side to the other.
+
 Relevant reading:
 
 - http://dwarffortresswiki.org/index.php/v0.34:Water
--- index.js
+++ index.js
@@ -54,46 +54,64 @@ function *range2d(x0, y0, x1, y1) {
     }
 }
 
+/**
+ * @param {Object[][]} matrix
+ * @returns {IterableIterator<Object>}
+ */
+function *iterateMatrix(matrix) {
+    const height = matrix.length;
+    const width = matrix[0].length;
+    for (const [x, y] of range2d(width, height)) {
+        yield matrix[x][y];
+    }
+}
+
 function trimRight(str) {
     return str.replace(/\s*$/, "");
 }
 
 class Tile {
-    constructor (symbol = "?") {
+    constructor (symbol, cssClass) {
         this.symbol = symbol;
+        this.cssClass = cssClass;
+        this.horizontalFlow = Math.trunc(Math.random() * 2);  // only applies to fluids
+    }
+
+    isWater() {
+        return this.symbol === Tile.WATER;
+    }
+
+    isWall() {
+        return this.symbol === Tile.WALL;
+    }
+
+    isEmpty() {
+        return this.symbol === Tile.EMPTY;
+    }
+
+    isNotEmpty() {
+        return this.symbol !== Tile.EMPTY;
     }
 
     static fromSymbol(symbol) {
         switch (symbol) {
             case undefined:
-            case " ": return new EmptyTile();
-            case "#": return new WallTile();
-            case "~": return new WaterTile();
+            case Tile.EMPTY: return new Tile(Tile.EMPTY, Tile.EMPTY_CSS);
+            case Tile.WALL: return new Tile(symbol, Tile.WALL_CSS);
+            case Tile.WATER: return new Tile(symbol, Tile.WATER_CSS);
             default: throw new Error(`Invalid character "${symbol}"`);
         }
     }
 }
 
-class WallTile extends Tile {
-    constructor () {
-        super("#");
-        this.cssClass = "wall";
-    }
-}
-
-class WaterTile extends Tile {
-    constructor () {
-        super("~");
-        this.cssClass = "water";
-    }
-}
-
-class EmptyTile extends Tile {
-    constructor () {
-        super(" ");
-        this.cssClass = "empty";
-    }
-}
+Tile.EMPTY = " ";
+Tile.WATER = "~";
+Tile.WALL = "#";
+Tile.EMPTY_CSS = "empty";
+Tile.WATER_CSS = "water";
+Tile.WALL_CSS = "wall";
+Tile.HORIZONTAL_FLOW_LEFT = 0;
+Tile.HORIZONTAL_FLOW_RIGHT = 1;
 
 class DwarfWater {
 
@@ -102,15 +120,77 @@ class DwarfWater {
         this.rawMap = rawMap;
         this.resetMap();
         this.resetTable();
-        this.step();
         this.stepFunction = this.step.bind(this);
-        setInterval(this.stepFunction, 1000);
+        setInterval(this.stepFunction, 500);
     }
 
     step() {
         for (const [x, y] of range2d(this.mapWidth, this.mapHeight)) {
-            // ToDo step cellular automaton
+            this.backingMap[x][y] = this.map[x][y];
         }
+
+        // iterate and update each cell - IMPORTANT: each cell can only write on its own position during its update
+        let dirtyTiles = [];
+        for (const [x, y] of range2d(this.mapWidth, this.mapHeight)) {
+            const tile = this.getTile(x, y);
+            if (tile.isWater()) {
+                const tileBelow = this.getTile(x, y + 1);
+
+                if (tileBelow && tileBelow.isEmpty()) {
+                    // water disappears if there's an empty space below it
+                    this.backingMap[x][y] = Tile.fromSymbol(Tile.EMPTY);
+                    dirtyTiles.push([x, y]);
+                }
+                // else if (tile.horizontalFlow === Tile.HORIZONTAL_FLOW_LEFT) {
+                //     // there's no space below it - try to flow horizontally
+                //     const tileLeft = this.getTile(x - 1, y);
+                //     if (tileLeft.isEmpty()) {
+                //         // disappear (will flow left)
+                //         this.backingMap[x][y] = Tile.fromSymbol(Tile.EMPTY);
+                //         dirtyTiles.push([x, y]);
+                //     } else {
+                //         // bumped into obstacle; change direction
+                //         this.backingMap[x][y].horizontalFlow = Tile.HORIZONTAL_FLOW_RIGHT;
+                //     }
+                // } else if (tile.horizontalFlow === Tile.HORIZONTAL_FLOW_RIGHT) {
+                //     // there's no space below it - try to flow horizontally
+                //     const tileRight = this.getTile(x + 1, y);
+                //     if (tileRight.isEmpty()) {
+                //         this.backingMap[x][y] = Tile.fromSymbol(Tile.EMPTY);
+                //         dirtyTiles.push([x, y]);
+                //     } else {
+                //         // bumped into obstacle; change direction
+                //         this.backingMap[x][y].horizontalFlow = Tile.HORIZONTAL_FLOW_LEFT;
+                //     }
+                // }
+            } else if (tile.isEmpty()) {
+                const tileAbove = this.getTile(x, y - 1);
+                if (tileAbove && tileAbove.isWater()) {
+                    // empty space becomes water if there's water above it
+                    this.backingMap[x][y] = Tile.fromSymbol(Tile.WATER);
+                    dirtyTiles.push([x, y]);
+                }
+            }
+        }
+
+        this.swapMapBuffers();
+
+        for (const [x, y] of dirtyTiles) {
+            DwarfWater.updateCell(this.tableCells[x][y], this.map[x][y]);
+        }
+    }
+
+    getTile(x, y) {
+        if (x >= 0 && x < this.mapWidth && y >= 0 && y < this.mapHeight) {
+            return this.map[x][y];
+        }
+        return undefined;
+    }
+
+    swapMapBuffers() {
+        const temp = this.map;
+        this.map = this.backingMap;
+        this.backingMap = temp;
     }
 
     resetMap() {
@@ -119,6 +199,8 @@ class DwarfWater {
         this.mapWidth = rawMapLines.reduce((maxWidth, line) => Math.max(maxWidth, line.length), 0);
         /** @type {Tile[][]} */
         this.map = Array.from(Array(this.mapWidth), () => Array(this.mapHeight));
+        /** @type {Tile[][]} */
+        this.backingMap = Array.from(Array(this.mapWidth), () => Array(this.mapHeight));
 
         for (const [x, y] of range2d(this.mapWidth, this.mapHeight)) {
             this.map[x][y] = Tile.fromSymbol(rawMapLines[y][x]);
@@ -143,15 +225,20 @@ class DwarfWater {
         for (const [x, y] of range2d(this.mapWidth, this.mapHeight)) {
             const tile = this.map[x][y];
             const cell = this.tableCells[x][y];
-            cell.innerText = tile.symbol;
-            for (const name of cell.classList.values()) cell.classList.remove(name);
-            cell.classList.add(tile.cssClass);
+            DwarfWater.updateCell(cell, tile);
         }
 
         if (!this.table.parentElement) {
             document.body.appendChild(this.table);
         }
     }
+
+    static updateCell(cell, tile) {
+        cell.innerText = tile.symbol;
+        for (const name of cell.classList.values()) cell.classList.remove(name);
+        cell.classList.add(tile.cssClass);
+    }
+
 
     static async run() {
         const rawMap = await getFile("map.txt");
